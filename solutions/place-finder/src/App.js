1831c8244432bd9678f646ff69e930148b14033e
Ehbraheem <ehbraheem@gmail.com>
2018-04-30 15:37:45 +0100

    Create Search component

--- solutions/place-finder/src/App.js
+++ solutions/place-finder/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Search from './components/containers/Search'
+
 class App extends Component {
   render() {
     return (
@@ -11,15 +13,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Place Finder</h1>
         </header>
-// https://codepen.io/huange/pen/rbqsD
-        <div class="wrap">
-   <div class="search">
-      <input type="text" class="searchTerm" placeholder="What are you looking for?"/>
-      <button type="submit" class="searchButton">
-        <i class="fa fa-search"></i>
-     </button>
-   </div>
-</div>
+(// https://codepen.io/huange/pen/rbqsD)
+        <Search />
       </div>
     );
   }
---
+++ solutions/place-finder/src/components/containers/Search.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+
+  render() {
+    return (
+       <div class="wrap">
+         <div class="search">
+            <input type="text" class="searchTerm" placeholder="What are you looking for?"/>
+            <button type="submit" class="searchButton">
+              <i class="fa fa-search"></i>
+           </button>
+         </div>
+      </div>
+    )
+  }
+}
+
